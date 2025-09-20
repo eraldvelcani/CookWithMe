cf712eac581eadab3fade8d550614ee145163b6e
eraldvelcani <eraldvelcani2003@gmail.com>
2025-09-20 04:22:49 +0200

    created Recipe type

--- app/routes/recipes/index.tsx
+++ app/routes/recipes/index.tsx
@@ -1,6 +1,17 @@
 import type { Route } from "./+types/index";
+import type { Recipe } from "~/types";
 
-const RecipesPage = () => {
+export async function loader({ request }:Route.LoaderArgs):Promise<{recipes:Recipe[]}> { //TS: Route.LoaderArgs is the type/shape of request. Promise is the return type.
+    const res = await fetch('http://localhost:8000/recipes');
+    const data = await res.json();
+
+    return { recipes: data };
+}
+
+const RecipesPage = ({ loaderData}:Route.ComponentProps) => { //the data returned from the function above is passed into the arguments here.
+    const { recipes } = loaderData as {recipes:Recipe[]};
+    console.log(recipes)
+
     return ( 
     <>
         <h2 className="text-3xl font-bold">🍳Recipes:</h2>
