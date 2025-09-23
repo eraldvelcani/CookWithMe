c4646b16db534180d7637dbd4a095118792b71ca
eraldvelcani <eraldvelcani2003@gmail.com>
2025-09-23 20:00:54 +0200

    started work on about page

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,10 +1,18 @@
 const AboutPage = () => {
     return ( 
-        <>
-            <h2 className="text-3xl font-bold text-white mb-2">
-                Let's get cooking!
-            </h2>
-        </>
+        <div className="max-w-5xl mx-auto px-6 py-16 bg-orange-50">
+            {/*Intro */}
+            <div className="flex flex-col md:flex-row md:items-start items-center gap-10 mb-12">
+                <img src="/images/logo-black-sm.png"/>
+                <div>
+                    <h1 className="text-4xl font-bold text-black mb-2">About Us</h1>
+                    <h2 className="text-3xl font-bold text-black mb-2">Who We Are</h2>
+                    <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptate delectus, eveniet quidem tenetur praesentium, at distinctio exercitationem mollitia dignissimos magnam. Recusandae, itaque. Quidem accusamus suscipit eaque ea consectetur iusto quisquam, sequi autem similique maiores ratione quos, atque perspiciatis perferendis rerum eligendi? Neque beatae tempore ducimus. Excepturi hic facilis harum pariatur quos ratione libero necessitatibus culpa, adipisci, mollitia illum quam!</p>
+                </div>
+            </div>
+            
+            
+        </div>
      );
 }
  
